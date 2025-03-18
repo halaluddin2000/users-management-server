91e09b4600c1598e729a5de5a45ec8c677654890
halaluddin2000 <helaluddin7001@gmail.com>
2025-03-19 03:17:14 +0600

    clint site data show

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 5000;
 // midule ower
 
 app.use(cors());
+app.use(express.json())
 
 
 const users = [
@@ -24,7 +25,13 @@ app.get('/users', (req, res) => {
     res.send(users)
 })
 app.post('/users', (req, res) => {
+    console.log('api hitting')
     console.log(req.body)
+
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser)
 })
 app.listen(port, () => {
     console.log(`server is running on PORT: ${port}`)
